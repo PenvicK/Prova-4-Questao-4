6051ad18549cfe644675dfd9df0fd8639e8007b2
[Vinicius Posser] <[viniciusposser37@gmail.com]>
2021-07-07 21:46:43 -0300

    first commit

---
+++ src/test/java/com/br/questao4/prova4/Prova4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.questao4.prova4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
